ec7690ac7e53557a75c8cebe19ecc38aee01559b
Antoine Rosenbach <github@rbch.ovh>
2022-08-28 08:59:55 +0200

    take credentials

--- temp.js
+++ temp.js
@@ -2,99 +2,104 @@ import fetch from "node-fetch";
 import * as Cheerio from "cheerio";
 import { dateDiffInDays } from "./utils.js";
 
-
-async function pretsNotLoggedIn(){
-    return fetch("https://bibliotheques.le-gresivaudan.fr/abonne/prets", {
-        "headers": {
-          "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-          "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
-          "cache-control": "max-age=0",
-          "sec-ch-ua": "\"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"104\"",
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": "\"macOS\"",
-          "sec-fetch-dest": "document",
-          "sec-fetch-mode": "navigate",
-          "sec-fetch-site": "none",
-          "sec-fetch-user": "?1",
-          "sec-gpc": "1",
-          "upgrade-insecure-requests": "1"
-        },
-        "referrerPolicy": "strict-origin-when-cross-origin",
-        "body": null,
-        "method": "GET"
-      });
+async function pretsNotLoggedIn(url) {
+  return fetch(`${url}/abonne/prets`, {
+    headers: {
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+      "cache-control": "max-age=0",
+      "sec-ch-ua":
+        '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"macOS"',
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "none",
+      "sec-fetch-user": "?1",
+      "sec-gpc": "1",
+      "upgrade-insecure-requests": "1",
+    },
+    referrerPolicy: "strict-origin-when-cross-origin",
+    body: null,
+    method: "GET",
+  });
 }
 
-function login(cookie) {
-    return fetch("https://bibliotheques.le-gresivaudan.fr/auth/login", {
-    "headers": {
-      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+function login(cookie, url, username, password) {
+  return fetch(`${url}/auth/login`, {
+    headers: {
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
       "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
       "cache-control": "max-age=0",
       "content-type": "application/x-www-form-urlencoded",
-      "sec-ch-ua": "\"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"104\"",
+      "sec-ch-ua":
+        '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
       "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": "\"macOS\"",
+      "sec-ch-ua-platform": '"macOS"',
       "sec-fetch-dest": "document",
       "sec-fetch-mode": "navigate",
       "sec-fetch-site": "same-origin",
       "sec-fetch-user": "?1",
       "sec-gpc": "1",
       "upgrade-insecure-requests": "1",
-      "cookie": `${cookie}; tarteaucitron=!analytics=wait`,
-      "Referer": "https://bibliotheques.le-gresivaudan.fr/abonne/prets",
-      "Referrer-Policy": "strict-origin-when-cross-origin"
+      cookie: `${cookie}; tarteaucitron=!analytics=wait`,
+      Referer: `${url}/abonne/prets`,
+      "Referrer-Policy": "strict-origin-when-cross-origin",
     },
-    "body": "username=L02136R249&password=1982&redirect=https%3A%2F%2Fbibliotheques.le-gresivaudan.fr%2Fabonne%2Fprets",
-    "method": "POST"
+    body: `username=${username}&password=${password}&redirect=https%3A%2F%2Fbibliotheques.le-gresivaudan.fr%2Fabonne%2Fprets`,
+    method: "POST",
   });
-  }
+}
 
-
-async function prets(cookie){
-    return fetch("https://bibliotheques.le-gresivaudan.fr/abonne/prets", {
-        "headers": {
-          "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-          "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
-          "cache-control": "max-age=0",
-          "sec-ch-ua": "\"Chromium\";v=\"104\", \" Not A;Brand\";v=\"99\", \"Google Chrome\";v=\"104\"",
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": "\"macOS\"",
-          "sec-fetch-dest": "document",
-          "sec-fetch-mode": "navigate",
-          "sec-fetch-site": "same-origin",
-          "sec-fetch-user": "?1",
-          "sec-gpc": "1",
-          "upgrade-insecure-requests": "1",
-          "cookie": `${cookie}; tarteaucitron=!analytics=wait`,
-          "Referer": "https://bibliotheques.le-gresivaudan.fr/abonne/prets",
-          "Referrer-Policy": "strict-origin-when-cross-origin"
-        },
-        "body": null,
-        "method": "GET"
-      });
+async function prets(cookie, url) {
+  return fetch(`${url}/abonne/prets`, {
+    headers: {
+      accept:
+        "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+      "accept-language": "fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7",
+      "cache-control": "max-age=0",
+      "sec-ch-ua":
+        '"Chromium";v="104", " Not A;Brand";v="99", "Google Chrome";v="104"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"macOS"',
+      "sec-fetch-dest": "document",
+      "sec-fetch-mode": "navigate",
+      "sec-fetch-site": "same-origin",
+      "sec-fetch-user": "?1",
+      "sec-gpc": "1",
+      "upgrade-insecure-requests": "1",
+      cookie: `${cookie}; tarteaucitron=!analytics=wait`,
+      Referer: `${url}/abonne/prets`,
+      "Referrer-Policy": "strict-origin-when-cross-origin",
+    },
+    body: null,
+    method: "GET",
+  });
 }
 
 function parseCookies(response) {
-    return response.headers.get('set-cookie').split(';')[0];
+  return response.headers.get("set-cookie").split(";")[0];
 }
 
-
-
-
 const extractData = (rawHTML) => {
-
   let headers = [];
   const $ = Cheerio.load(rawHTML);
   $("table.loans thead tr th").each((i, th) =>
     headers.push($(th).text().trim().toLowerCase().replace(/ /gi, "_"))
   );
   headers = headers.map((header) =>
-    header === "vignette" ? "cover"
-      : header === "retour_prévu" ? "date"
-      : header === "bibliothèque" ? "library" 
-      : header === "titre" ? "title"
-      : header === "emprunté_par" ? "borrower"
+    header === "vignette"
+      ? "cover"
+      : header === "retour_prévu"
+      ? "date"
+      : header === "bibliothèque"
+      ? "library"
+      : header === "titre"
+      ? "title"
+      : header === "emprunté_par"
+      ? "borrower"
       : header
   );
   // go through cells
@@ -129,15 +134,14 @@ const extractData = (rawHTML) => {
   });
 };
 
-const collectData = async (credential) => { 
-
-    // HTTP Requests
-    const blankRequest = await pretsNotLoggedIn();
-    const cookie = parseCookies(blankRequest);
-    await login(cookie);
-    const response = await prets(cookie);
-    const rawHTML = await response.text();
-    // End HTTP Requests
+const collectData = async (credential) => {
+  // HTTP Requests
+  const blankRequest = await pretsNotLoggedIn(credential.url);
+  const cookie = parseCookies(blankRequest);
+  await login(cookie, credential.url, credential.username, credential.password);
+  const response = await prets(cookie, credential.url);
+  const rawHTML = await response.text();
+  // End HTTP Requests
 
   const extractedData = extractData(rawHTML);
 
@@ -162,21 +166,23 @@ const collectData = async (credential) => {
   });
 };
 
+const injectLastRun = (data) =>
+  data.reduce(
+    (acc, next) => ({
+      lastRun: acc.lastRun,
+      data: [...acc.data, next],
+    }),
+    {
+      lastRun: new Date().toLocaleString("en-US", { timeZone: "Europe/Paris" }),
+      data: [],
+    }
+  );
 
-
-const injectLastRun = data => data.reduce((acc, next) => ({
-	lastRun: acc.lastRun,
-	data: [...acc.data, next]
-}),{
-	lastRun: new Date().toLocaleString('en-US', { timeZone: 'Europe/Paris' }),
-	data: []
-});
-
-const cacheSave = memjsClient => data => {
-	memjsClient.set("loans", JSON.stringify(data), { expires: 60 * 60 * 24 });
-	return data;
-}
-const sortBy = field => data => data.sort((a, b) => a[field] - b[field])
+const cacheSave = (memjsClient) => (data) => {
+  memjsClient.set("loans", JSON.stringify(data), { expires: 60 * 60 * 24 });
+  return data;
+};
+const sortBy = (field) => (data) => data.sort((a, b) => a[field] - b[field]);
 
 export default class DataFetcher {
   constructor(memjsClient, credentials) {
@@ -193,9 +199,22 @@ export default class DataFetcher {
   }
 }
 
-
 ////////// TEST
-const creds = [{name:'toto'}];
-const dataFetcher = new DataFetcher({set: function(){}},creds)
+const creds = [
+  {
+    name: "Meylan",
+    url: "https://www.meylan-bibliotheque.fr",
+    username: "0052537",
+    password: "2012",
+  },
+  {
+    name: "Montbonnot",
+    url: "https://bibliotheques.le-gresivaudan.fr",
+    username: "L02136R249",
+    password: "1982",
+  },
+];
+const dataFetcher = new DataFetcher({ set: function () {} }, creds);
 const result = await dataFetcher.run();
-console.log(JSON.stringify(result));
+console.log(result);
+// console.log(JSON.stringify(result));
