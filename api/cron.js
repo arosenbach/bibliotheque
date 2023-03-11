6f982fc0d45dd06cee5090008cdec68765236931
Antoine Rosenbach <github@rbch.ovh>
2023-03-11 18:28:52 +0100

    try cron yarn run:all

--- api/cron.js
+++ api/cron.js
@@ -1,3 +1,14 @@
 export default function handler(req, res) {
+  const { exec } = require("child_process");
+  exec("yarn run:all", (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    // the *entire* stdout and stderr (buffered)
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+  });
   res.status(200).end("Hello Cron!");
 }
